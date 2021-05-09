38489c70b9232f03ef8b8d0004776efe50883c22
lucfonjallaz <luc.fonjallaz@2bit.ch>
2021-05-09 18:15:02 +0200

    cleanup unused imports

--- backend/src/test/kotlin/ch/lucfonjallaz/drezip/BaseIntegrationTest.kt
+++ backend/src/test/kotlin/ch/lucfonjallaz/drezip/BaseIntegrationTest.kt
@@ -1,12 +1,7 @@
 package ch.lucfonjallaz.drezip
-import org.junit.jupiter.api.BeforeAll
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.ActiveProfiles
-import org.testcontainers.containers.MySQLContainer
-import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
