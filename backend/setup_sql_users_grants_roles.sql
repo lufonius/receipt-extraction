d7604dc5566dde5b9fe4945c695cd1891310bf8a
lucfonjallaz <luc.fonjallaz@2bit.ch>
2021-05-26 21:52:29 +0200

    make setup database idempotent, set all users assigned roles activated by default

--- backend/setup_sql_users_grants_roles.sql
+++ backend/setup_sql_users_grants_roles.sql
@@ -1,14 +1,16 @@
-CREATE USER 'drezip_app'@'%' IDENTIFIED BY 'drezip_app';
-CREATE USER 'drezip_flyway'@'%' IDENTIFIED BY 'drezip_flyway';
+CREATE SCHEMA IF NOT EXISTS drezip;
 
-CREATE ROLE ddl;
-GRANT DROP, CREATE, ALTER ON test_drezip.* TO ddl;
+CREATE USER IF NOT EXISTS 'drezip_app'@'%' IDENTIFIED BY 'drezip_app';
+CREATE USER IF NOT EXISTS 'drezip_flyway'@'%' IDENTIFIED BY 'drezip_flyway';
 
-CREATE ROLE dml;
-GRANT INSERT, UPDATE, DELETE ON test_drezip.* TO dml;
+CREATE ROLE IF NOT EXISTS ddl;
+GRANT DROP, CREATE, ALTER, REFERENCES ON drezip.* TO ddl;
 
-CREATE ROLE dql;
-GRANT SELECT ON test_drezip.* TO dql;
+CREATE ROLE IF NOT EXISTS dml;
+GRANT INSERT, UPDATE, DELETE ON drezip.* TO dml;
+
+CREATE ROLE IF NOT EXISTS dql;
+GRANT SELECT ON drezip.* TO dql;
 
 GRANT ddl TO 'drezip_flyway'@'%';
 GRANT dml TO 'drezip_flyway'@'%';
@@ -16,3 +18,6 @@ GRANT dql TO 'drezip_flyway'@'%';
 
 GRANT dml TO 'drezip_app'@'%';
 GRANT dql TO 'drezip_app'@'%';
+-- roles need to be activated when the user logs in
+-- with this statement, all assigned roles are activated by default
+SET DEFAULT ROLE ALL TO 'drezip_flyway'@'%','drezip_app'@'%';
