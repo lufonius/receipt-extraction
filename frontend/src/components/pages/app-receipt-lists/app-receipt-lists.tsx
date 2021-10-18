04163e7f28327f1e11ead3376f823539247bd8e4
lucfonjallaz <luc.fonjallaz@2bit.ch>
2021-10-18 13:00:14 +0200

    redesign home

--- frontend/src/components/pages/app-receipt-lists/app-receipt-lists.tsx
+++ frontend/src/components/pages/app-receipt-lists/app-receipt-lists.tsx
@@ -7,6 +7,7 @@ import {Mapper} from "../../model/mapper";
 import {RouterHistory} from "@stencil/router";
 import {CategoryService} from "../category.service";
 import {ReceiptService} from "../receipt.service";
+import {Icons} from "../../../global/material-icons-enum";
 
 @Component({
   tag: 'app-receipt-lists',
@@ -71,19 +72,43 @@ export class AppReceiptLists {
   render() {
     return (
       <Host>
-          {this.receiptListHasBeenLoaded && <div>
-          <span>you have {this.countReceipts(ReceiptStatusDto.Uploaded)} receipts uploaded</span>
-            <app-button onPress={() => this.continueEditingNextReceipt(ReceiptStatusDto.Uploaded)} primary>Continue editing</app-button>
-            <br />
+          <div class="content-wrapper">
+            <div class="header">
+              <h1>Home</h1>
+              <p>Use drezip to upload receipts, extract the items and categorise them.</p>
+            </div>
+            <div class="body">
+              {this.receiptListHasBeenLoaded && <div>
 
-            <span>you have {this.countReceipts(ReceiptStatusDto.Open)} receipts open</span>
-            <app-button onPress={() => this.continueEditingNextReceipt(ReceiptStatusDto.Open)} primary>Continue editing</app-button>
-            <br />
+                <div class="status-container">
+                  <h1>{this.countReceipts(ReceiptStatusDto.Uploaded)}</h1>
+                  <h4>Receipts uploaded</h4>
+                  <app-button onPress={() => this.continueEditingNextReceipt(ReceiptStatusDto.Uploaded)} primary>
+                    crop receipts
+                    <app-icon icon={Icons.ARROW_RIGHT} />
+                  </app-button>
+                </div>
 
-            <span>you have {this.countReceipts(ReceiptStatusDto.InProgress)} receipts in progress</span>
-            <app-button onPress={() => this.continueEditingNextReceipt(ReceiptStatusDto.InProgress)} primary>Continue editing</app-button>
-            <br />
-          </div>}
+                <div class="status-container">
+                  <h1>{this.countReceipts(ReceiptStatusDto.Open)}</h1>
+                  <h4>Receipts ready for extraction</h4>
+                  <app-button onPress={() => this.continueEditingNextReceipt(ReceiptStatusDto.Open)} primary>
+                    Start extraction
+                    <app-icon icon={Icons.ARROW_RIGHT} />
+                  </app-button>
+                </div>
+
+                <div class="status-container">
+                  <h1>{this.countReceipts(ReceiptStatusDto.InProgress)}</h1>
+                  <h4>Receipts in progress</h4>
+                  <app-button onPress={() => this.continueEditingNextReceipt(ReceiptStatusDto.InProgress)} primary>
+                    Continue extraction
+                    <app-icon icon={Icons.ARROW_RIGHT} />
+                  </app-button>
+                </div>
+              </div>}
+            </div>
+          </div>
 
         <app-navbar activeUrl="/" />
       </Host>
