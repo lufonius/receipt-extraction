9604df8d249af7dd66b6922a209db2d6afdf04b9
lucfonjallaz <luc.fonjallaz@2bit.ch>
2021-12-22 14:55:28 +0100

    make test and local development independent of azure infrastructure

---
+++ backend/src/test/kotlin/ch/lucfonjallaz/drezip/TestDrezipApplication.kt
@@ -0,0 +1,25 @@
+package ch.lucfonjallaz.drezip
+
+import ch.lucfonjallaz.drezip.mocks.MockConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
+
+// separate test app so that we do not clutter the productive code with
+// conditional activation of mocks depending on the profile
+// also, the productive JAR does not contain test / local code
+@SpringBootApplication
+@Import(
+	SwaggerConfiguration::class,
+	RequestLoggingConfiguration::class,
+	SecurityConfiguration::class,
+	MockConfiguration::class
+)
+class Application {
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			runApplication<Application>(*args)
+		}
+	}
+}
